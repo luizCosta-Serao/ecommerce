b2093534d488253f21c19ae08884018e32894e0a
Luiz Serão <luiz.serao@hotmail.com>
2024-07-20 08:03:45 -0300

    pagamento com cartão via pagseguro

---
+++ js/index.js
@@ -0,0 +1,58 @@
+$(function() {
+  let publicKey = '';
+  let valor = '';
+  let images = [];
+
+  // Pagamento com cartão
+  $('#formCard').submit(function(e) {
+    e.preventDefault()
+    let card = PagSeguro.encryptCard({
+      publicKey: $('#publicKey').val(),
+      holder: $('#nome_cartao').val(),
+      number: $('#numero_cartao').val(),
+      expMonth: $('#mes_validade').val(),
+      expYear: $('#ano_validade').val(),
+      securityCode: $('#cvv').val(),
+    })
+    let encrypted = card.encryptedCard;
+    $('#encryptedCard').val(encrypted);
+    $.ajax({
+      url: 'http://localhost/ecommerce/ecommerce_php/ajax/cartao.php',
+      method: 'post',
+      data: {
+        encryptedCard: encrypted
+      }
+    })
+  })
+
+  // Obtendo a chave publica para pagamento com cartão
+  $.ajax({
+    url: 'http://localhost/ecommerce/ecommerce_php/ajax/finalizar-pagamento.php',
+    method: 'post',
+    dataType: 'json'
+  }).done(function(data) {
+    $('#publicKey').val(data.public_key);
+    publicKey = data.public_key;
+  })
+
+  /*
+  $('form').submit(function(e) {
+    e.preventDefault();
+    disabledForm();
+  })
+
+  function disabledForm() {
+    $('form').animate({'opacity': '.4'});
+    $('form').find('input').attr('disabled', 'disabled');
+    $('form').find('select').attr('disabled', 'disabled');
+  }
+
+  function enableForm() {
+    $('form').animate({'opacity': '1'});
+    $('form').find('input').removeAttr('disabled')
+    $('form').find('select').removeAttr('disabled')
+  }
+    */
+})
+
+
--- pages/finalizar-pedido.php
+++ pages/finalizar-pedido.php
@@ -29,5 +29,42 @@
 
 <section class="finalizar-pedido">
   <h2>Total: R$ <?php echo number_format($total, 2, ',', '.') ?></h2>
-  <a href="" class="btn-pagamento">Pagar Agora!</a>
+  <form name="formCard" id="formCard" method="post">
+    <input type="text" name="publicKey" id="publicKey">
+    <input type="text" name="encryptedCard" id="encryptedCard">
+    <!--
+    <label for="fullname">Nome Completo</label>
+    <input type="text" name="fullname" id="fullname">
+
+    <label for="cpf">CPF</label>
+    <input type="text" name="cpf" id="cpf">
+
+    <label for="bandeira">Bandeiras</label>
+    <select name="bandeira" id="bandeira">
+      <option value="visa">Visa</option>
+    </select>
+
+    <label for="valor">Valor</label>
+    <select name="valor" id="valor">
+      <option value="199.00">1x de R$ 199.00</option>
+    </select>
+      -->
+
+    <label for="numero_cartao">Número do Cartão</label>
+    <input type="text" name="numero_cartao" id="numero_cartao">
+
+    <label for="nome_cartao">Nome no Cartão</label>
+    <input type="text" name="nome_cartao" id="nome_cartao">
+
+    <label for="cvv">CVV</label>
+    <input type="text" name="cvv" id="cvv">
+
+    <label for="mes_validade">Mês de Validade</label>
+    <input type="text" name="mes_validade" id="mes_validade">
+
+    <label for="ano_validade">Ano de Validade</label>
+    <input type="text" name="ano_validade" id="ano_validade">
+
+    <input type="submit" class="btn-pagamento" name="enviar" id="enviar" value="Enviar Pagamento">
+  </form>
 </section>
